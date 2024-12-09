5bebd096762e45005cdbda0c90039235a485e839
Xuefen <215437588@qq.com>
2024-12-09 14:42:16 +0800

    Update eval shells

---
+++ eval_fedavg.sh
@@ -0,0 +1,14 @@
+mkdir eval_logs
+
+pip install scikit-learn
+
+python main_fed_LNL.py \
+--dataset cifar10 \
+--model resnet18 \
+--epochs 120 \
+--noise_type_lst symmetric pairflip \
+--noise_group_num 50 50 \
+--group_noise_rate 0.0 0.4 0.0 0.4 \
+--partition dirichlet \
+--dd_alpha 1.0 \
+--method fedavg | tee ./eval_logs/fedavg_mixed04_dirichlet10.txt
--- reference.sh
+++ reference.sh
@@ -7,14 +7,26 @@ python main_fed_LNL.py \
 --dataset cifar10 \
 --model resnet18 \
 --epochs 120 \
---noise_type_lst symmetric \
---noise_group_num 100  \
---group_noise_rate 0.0 0.4 \
+--noise_type_lst symmetric pairflip \
+--noise_group_num 50 50 \
+--group_noise_rate 0.0 0.4 0.0 0.4 \
 --partition dirichlet \
 --dd_alpha 1.0 \
---epoch_of_stage1 20 \
---lamda_pencil 1000 \
---method fedELC | tee ./fedELC_logs/symm04_dirichlet10.txt
+--method fedELC | tee ./fedELC_logs/mixed04_dirichlet10.txt
+
+
+# python main_fed_LNL.py \
+# --dataset cifar10 \
+# --model resnet18 \
+# --epochs 120 \
+# --noise_type_lst symmetric \
+# --noise_group_num 100  \
+# --group_noise_rate 0.0 0.4 \
+# --partition dirichlet \
+# --dd_alpha 1.0 \
+# --epoch_of_stage1 20 \
+# --lamda_pencil 1000 \
+# --method fedELC | tee ./fedELC_logs/symm04_dirichlet10.txt
 
 # python main_fed_LNL.py \
 # --dataset cifar10 \
@@ -28,14 +40,3 @@ python main_fed_LNL.py \
 # --epoch_of_stage1 20 \
 # --lamda_pencil 1000 \
 # --method fedELC | tee ./fedELC_logs/pair04_dirichlet10.txt
-
-# python main_fed_LNL.py \
-# --dataset cifar10 \
-# --model resnet18 \
-# --epochs 120 \
-# --noise_type_lst symmetric pairflip \
-# --noise_group_num 50 50 \
-# --group_noise_rate 0.0 0.4 0.0 0.4 \
-# --partition dirichlet \
-# --dd_alpha 1.0 \
-# --method fedELC | tee ./fedELC_logs/mixed04_dirichlet10.txt
